7a8b93ac17435fa826efb178a2acb4687f38592d
anitacheung83 <anitacheung83@hotmail.com>
2023-09-03 21:56:24 -0400

    Fix course grid height

--- src/components/CourseGrid.tsx
+++ src/components/CourseGrid.tsx
@@ -31,7 +31,7 @@ export default function CourseGrid(props: CourseGridProps) {
         // } else {
         //     return props.courseGridHeight - 0.6
         // }
-        return (props.courseGridHeight * props.height) - 1 - (0.2 * props.height)
+        return (props.courseGridHeight * props.height) - 0.8 - (0.2 * props.height)
     }
 
     let courseGridClassName = `${CourseGridCSS.courseGrid} ${CourseGridCSS.center}`;
@@ -41,7 +41,7 @@ export default function CourseGrid(props: CourseGridProps) {
             style={{
                 backgroundColor: props.backgroundColor,
                 height: calculateHeight(),
-                width: props.courseGridWidth - 0.8,
+                width: props.courseGridWidth - 0.4,
                 top: props.top * props.courseGridHeight
             }}>
             <p className={`${CourseGridCSS.courseInput} ${CourseGridCSS.courseCode}`}>{props.courseCode}</p>
