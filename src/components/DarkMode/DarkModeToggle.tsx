3fb750c582f83a067b9a0b89a1733c40e7dfce96
anitacheung83 <anitacheung83@hotmail.com>
2023-11-08 11:02:35 -0500

    Added tests and documentation for dark mode toggle

--- src/components/DarkMode/DarkModeToggle.tsx
+++ src/components/DarkMode/DarkModeToggle.tsx
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDarkModeContext } from "../../context/DarkModeContext";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import IconButton from "@mui/material/IconButton";
 
+/**
+ * DarkModeToggle component is a user interface element for toggling between dark and light modes.
+ *
+ * @component
+ */
 export default function DarkModeToggle() {
-    const { darkMode, setDarkMode } = useDarkModeContext()
+    // Access the darkMode state and setDarkMode function from the DarkModeContext
+    const { darkMode, setDarkMode } = useDarkModeContext();
 
     return (
         <>
             {darkMode ?
-                <IconButton color="info">
+                // Render a button for switching to light mode when dark mode is enabled
+                <IconButton color="info" data-testid="light-mode">
                     <LightModeIcon onClick={() => setDarkMode(!darkMode)} />
                 </IconButton> :
-                <IconButton color="info">
+                // Render a button for switching to dark mode when light mode is enabled
+                <IconButton color="info" data-testid="dark-mode">
                     <DarkModeIcon onClick={() => setDarkMode(!darkMode)} />
                 </IconButton>
             }
