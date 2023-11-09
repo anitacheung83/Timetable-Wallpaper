ee9d4bb2126d9dce310770779d56f650f2a56d06
anitacheung83 <anitacheung83@hotmail.com>
2023-11-09 09:32:24 -0500

    Updated img quality

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,6 +6,7 @@ import SchoolIsFactoryBlackLogo from "../assets/SchoolIsFactoryBlackLogo.png";
 import LandingPageIphone from "../assets/LandingPageIphone.png"
 import LandingPageIpad from "../assets/LandingPageIpad.png"
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer/Footer";
 
 interface LandingPageProps {
     handleCreateNow: () => void
@@ -17,24 +18,25 @@ export default function LandingPage(props: LandingPageProps) {
     }
     return (
         <>
-            <Navbar />
-            <Grid container direction="row" sx={{ height: "90vh", overflow: "hidden" }}>
-                <Grid item xs={12} md={6} direction="column" className="center">
-                    <div style={{ padding: "0vw 4vw 10vw 8vw" }}>
-                        <Typography variant="h3" sx={{ py: "10vh" }}> Design Timetables That Perfectly Fit Your Iphone and Ipad Lockscreen</Typography>
-                        <Button variant="outlined" color="info" onClick={handleCreateNow}> Create Now</Button>
-                    </div>
+            <div>
+                <Navbar />
+                <Grid container direction="row" sx={{ minHeight: "90vh", overflow: "hidden" }}>
+                    <Grid item xs={12} md={6} direction="column" className="center">
+                        <div style={{ padding: "0vw 4vw 10vw 8vw" }}>
+                            <Typography variant="h3" sx={{ py: "10vh" }}> Design Timetables That Perfectly Fit Your Iphone and Ipad Lockscreen</Typography>
+                            <Button variant="outlined" color="info" onClick={handleCreateNow}> Create Now</Button>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} md={6} style={{ display: "flex", alignItems: "center" }}>
+                        <div style={{ position: "relative" }}>
+                            <img src={LandingPageIphone} alt="Landing Page Iphone" height="600px" style={{ position: "absolute", left: "-320px", top: "190px" }} />
+                            <img src={LandingPageIpad} alt="Landing Page Ipad" height="820px" />
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={6} style={{ display: "flex", alignItems: "center" }}>
-                    <div style={{ position: "relative" }}>
-                        <img src={LandingPageIphone} alt="Landing Page Iphone" height="600px" style={{ position: "absolute", left: "-320px", top: "190px" }} />
-                        <img src={LandingPageIpad} alt="Landing Page Ipad" height="820px" />
-                    </div>
-                </Grid>
-            </Grid>
+                <Footer />
 
-
-
+            </div>
         </>
     )
 }
